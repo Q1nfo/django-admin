c1efddff39b45d6db4f032d377e2df2d850a631e
q1nfo <q1nffos@gmail.com>
2023-11-14 19:06:55 +0300

    complete tests-docs

--- src/config/test_settings.py
+++ src/config/test_settings.py
@@ -109,5 +109,3 @@ class Settings(TestCase):
         ).delete()
 
 
-
-
--- src/movies/models.py
+++ src/movies/models.py
@@ -3,7 +3,6 @@ import uuid
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
